c28a8dd5181fefca08bdeff233f81d2e49f124b0
Abhay <abhaymaurya821@gmail.com>
2025-09-24 09:50:23 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+
+import Hero from './pages/Hero';
+import Card from './pages/Card';
+import Cal from './pages/Cal';
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Hero />
+      <Card/>
+      <Cal/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Cal.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const Cal = () => {
+  return (
+    <>
+     
+     
+      <section className="bg-[#f5f5f4] mt-30  ">
+        <div className="border  max-w-7xl flex items-center justify-center mt-30 m-auto ">
+          <div className="">
+            <span className="">
+            Your Inbox, Calendar & Slack – finally working together
+            </span>
+            <span className="">
+              <p>Merlin syncs Gmail, Google Calendar, and Slack so you can book trips, clear tasks, and silence chaos from one chat.</p>
+            </span>
+          </div>
+        </div>
+
+     
+     <section className="relative flex flex-col md:flex-row justify-between items-start mt-90 gap-6 max-w-6xl mx-auto px-4 sm:px-6 py-8 sm:py-12 rounded-3xl sm:rounded-4xl mb-8 sm:mb-12 border  ">
+      <div className="flex flex-col items-start gap-4 sm:gap-6 w-full">
+        <div>
+          <h3 className="text-white text-2xl sm:text-3xl md:text-4xl font-bold leading-snug mb-2 sm:mb-4 max-w-full md:max-w-[480px]">
+            
+          </h3>
+          <p className="text-white text-base sm:text-lg leading-relaxed">
+          
+          </p>
+        </div>
+      </div>
+      
+    </section>
+
+    <section className="flex items-center justify-center px-2 sm:px-0 ">
+      <div className="max-w-6xl w-full mx-auto rounded-2xl grid grid-cols-1 md:grid-cols-2 gap-4  ">
+        
+        <div className="border  rounded-3xl sm:rounded-4xl flex flex-col items-center">
+         
+          <div className="items-end p-4 sm:p-6">
+            
+            
+          </div>
+        </div>
+        <div className="border  rounded-3xl sm:rounded-4xl flex flex-col items-center">
+          
+          <div className="items-end p-4 sm:p-6">
+            <h1 className="text-white text-2xl sm:text-3xl md:text-[2.75rem]  font-bold"></h1>
+            <p className="text-white text-base sm:text-lg mt-3">
+            
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    </section>
+    </>
+  );
+};
+
+export default Cal;
